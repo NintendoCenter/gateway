b7ae3ced8ddd28bd4a97e6b3e437ed918c5a8af0
Elijah Zobenko <i.zobenko@corp.mail.ru>
2021-08-11 18:27:58 +0300

    Move server's logic to main

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
 
+	"NintendoCenter/gateway/graph/generated"
+	"NintendoCenter/gateway/graph/resolvers"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolvers.Resolver{}}))
+
+	http.Handle("/playground", playground.Handler("GraphQL playground", "/"))
+	http.Handle("/", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
